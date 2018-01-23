09494664f4a5f0a52bfc5924b1aca2d8da5df1fe
Katrina Kalantar <kkalantar@berkeley.edu>
2018-01-23 13:37:03 -0800

    fix typo

--- align_and_convert_reference_microbe.sh
+++ align_and_convert_reference_microbe.sh
@@ -8,4 +8,4 @@ echo $1.fasta
 echo "Running GSNAPL-1000"
 /data/bin/gsnap-2015-12-31-bin/bin/gsnapl -A m8 --batch=4 --npaths=1000 --nofails --ordered -t 16 --maxsearch=1001 --max-mismatches=20 -D /nvme/databases/gsnap/nt-Jul2015-mask350-k16/nt_mask350-k16/ -d nt_mask350-k16 $1.fasta > $1.1000.m8
 echo "Running phylowalk 1000"
-python3 ./scripts/phylowalk_m8parse.py -i $1.1000.m8 -o $1.1000.m8.lca
+python3 ./scripts/phylowalk_m8parser.py -i $1.1000.m8 -o $1.1000.m8.lca
